8723df8ac9342d6582295d99f929c9f874db9d85
Manish Srivastava <mannu927@yahoo.com>
2026-03-01 12:22:17 +0530

    Update shap_explainability.py

--- ML-Models/Decision-Tree/Backorder-Prediction/advanced_models/shap_explainability.py
+++ ML-Models/Decision-Tree/Backorder-Prediction/advanced_models/shap_explainability.py
@@ -11,3 +11,24 @@ def shap_analysis(model, X_train, X_test):
     shap_values = explainer.shap_values(X_test)
 
     return shap_values, explainer
+
+
+def shap_summary_plot(shap_values, X_test):
+
+    shap.summary_plot(shap_values, X_test)
+
+    plt.savefig("../results/shap_summary.png")
+
+    plt.show()
+
+
+def shap_force_plot(explainer, shap_values, X_test):
+
+    shap.initjs()
+
+    shap.force_plot(
+        explainer.expected_value,
+        shap_values[0],
+        X_test.iloc[0]
+    )
+

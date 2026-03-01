c3448a1d871ea72cb4145d19c841aba868a9df07
Manish Srivastava <mannu927@yahoo.com>
2026-03-01 12:09:00 +0530

    Update evaluate.py

--- ML-Models/Decision-Tree/Backorder-Prediction/src/evaluate.py
+++ ML-Models/Decision-Tree/Backorder-Prediction/src/evaluate.py
@@ -1,1 +1,22 @@
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def evaluate_model(model, X_test, y_test):
+
+    y_pred = model.predict(X_test)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+
+    print("\nClassification Report\n")
+    print(classification_report(y_test, y_pred))
+
+    cm = confusion_matrix(y_test, y_pred)
+
+    sns.heatmap(cm, annot=True, fmt="d")
+    plt.title("Confusion Matrix")
+    plt.show()

9985bbcb8133127c47d265571b7f0881be16ad0c
Manish Srivastava <mannu927@yahoo.com>
2026-03-01 12:16:25 +0530

    Implementing XGBoost model

--- ML-Models/Decision-Tree/Backorder-Prediction/advanced_models/xgboost_model.py
+++ ML-Models/Decision-Tree/Backorder-Prediction/advanced_models/xgboost_model.py
@@ -1,1 +1,19 @@
+! pip install xgboost
 
+
+from xgboost import XGBClassifier
+
+def train_xgboost(X_train, y_train):
+
+    model = XGBClassifier(
+        n_estimators=200,
+        max_depth=6,
+        learning_rate=0.1,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    return model
